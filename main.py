4dc6bd2aab9f9e3e831ec61825a70bb463664e95
Neffy <ihavenofunnyname@gmail.com>
2023-05-28 21:10:06 +1000

    Lobby: Turn URLs into working buttons

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import os
-import urllib
-import requests
 from dotenv import load_dotenv
 
 import discord
@@ -9,6 +7,7 @@ from discord.ext import commands
 # Do I have to do this? Is there a way to like have python default import a .py? maybe that's what __init__.py is for?
 # I don't understand how this wasn't immediately answered via google.
 from settings.settings import Settings
+from lobby.lobby import Lobby
 
 def run(token):
     intents = discord.Intents.default()
@@ -21,22 +20,8 @@ def run(token):
     async def on_ready():
         print(f"Logged in as {bot.user}\nSetting up cogs")
         await bot.add_cog(Settings(bot))
+        await bot.add_cog(Lobby(bot))
         print('Set up cogs')
-
-    def shorten(url_long):
-            url = 'http://tinyurl.com/api-create.php?' + urllib.parse.urlencode({"url": url_long})
-            res = requests.get(url)
-            return res.text
-
-    @bot.command()
-    async def test(ctx):
-        class SimpleView(discord.ui.View):
-            def __init__(self):
-                super().__init__(timeout=30)  # times out after 30 seconds
-                button = discord.ui.Button(label='Join Lobby', style=discord.ButtonStyle.url, url=shorten('steam://joinlobby/245170/109775244914157053/76561198046191506'))
-                self.add_item(button)
-        await ctx.send('Working lobby link because discord sucks', view=SimpleView())
-
     bot.run(token)
 
 if __name__ == "__main__":
