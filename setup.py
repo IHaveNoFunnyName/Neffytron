cd81c107c7b760a0a2f53458a934ef6f563a3b2f
Micah Bradbury <micahnbrad@gmail.com>
2023-09-11 22:13:25 +1000

    convert code into neffytron module

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="neffytron",
+    version="0.1",
+    description="A discord bot for Neffytron",
+    url="",
+    author="",
+    author_email="",
+    license="MIT",
+    packages=find_packages(),
+    zip_safe=False,
+)
