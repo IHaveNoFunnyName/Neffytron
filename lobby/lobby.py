39e67748be32be4d6e2bddcddb20e889607bd320
Neffy <ihavenofunnyname@gmail.com>
2025-09-19 09:05:30 +1000

    Fix(Lobby): Use my own redirect over tinyurl

--- lobby/lobby.py
+++ lobby/lobby.py
@@ -4,15 +4,10 @@ from discord.ext.commands import Cog
 import discord
 import re
 
-def shorten(url_long):
-    url = 'http://tinyurl.com/api-create.php?' + urllib.parse.urlencode({"url": url_long})
-    res = requests.get(url)
-    return res.text
-
 class SimpleView(discord.ui.View):
     def __init__(self, link):
         super().__init__()
-        button = discord.ui.Button(label='Working lobby link because discord sucks', style=discord.ButtonStyle.url, url=shorten(link))
+        button = discord.ui.Button(label='Working lobby link because discord sucks', style=discord.ButtonStyle.url, url=link.replace("steam://joinlobby/", "https://neffytron.com/lobby/"))
         self.add_item(button)
 
 class Lobby(Cog):
@@ -21,7 +16,7 @@ class Lobby(Cog):
 
     @Cog.listener("on_message")
     async def lobby_link(self, message):
-        match = re.search('(steam:\/\/[^\s]*)', message.content)
+        match = re.search('(steam:\/\/joinlobby\/[^\s]*)', message.content)
         if match:
             await message.channel.send('', view=SimpleView(match.group(0)))
     
